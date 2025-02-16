4c42188db5a8d38d3a44ec697c133445740f9651
Kerstin Hinteregger <kerstinmarhi@edu.aau.at>
2025-02-16 19:52:42 +0100

    Added missing method declaration in header.

--- include/Game.h
+++ include/Game.h
@@ -1,29 +1,28 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "Dungeon.h"
 #include "Player.h"
 #include "Room.h"
-#include <vector>
-#include "Dungeon.h"
 #include <memory>
-
+#include <vector>
 
 class Game {
 private:
     std::vector<std::unique_ptr<Room>> rooms;
     Room* currentRoom;
-    Player player; 
+    Player player;
     Dungeon dungeon;
 
 public:
     Game();
     ~Game();
     void start();
-    void mainMenu(); 
-    void explore(); 
+    void mainMenu();
+    void explore();
     void initializeRooms();
     void combat(Monster* monster);
-
+    void resetGame();
 };
 
 #endif
